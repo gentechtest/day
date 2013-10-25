b0f09fe95dbe8e7155e8978a94871badf285e43b
Vadim <baklamakla@gmail.com>
2013-10-25 14:38:32 +0300

    Update PlayMusic class

--- src/content/PlayMusicOnLand.java
+++ src/content/PlayMusicOnLand.java
@@ -10,22 +10,29 @@ public class PlayMusicOnLand extends BasicTestCase {
 	 //Play music on a page
 	@Test(priority=1, description="Проверяет играет ли трек на странице скачки")
 	public void PlayMusic () throws Exception {
-		 driver.get(musicUrl);
-		 playMusic();
+	   driver.get(musicUrl);
+	   assertPage(musicUrl);
+	   playMusic();
 }
 	   //Play music test
 	   public void playMusic () throws Exception {
 	   driver.findElement(By.xpath("//*[@class='row']/strong/a")).click();
-	   driver.findElement(By.xpath("//*[@class='mainsection']//*[@class='advanced_playlist']/li/a")).click();
+	   driver.findElement(By.xpath("//*[@class='mainsection']//*[@class='advanced_playlist']/li/a[1]")).click();
+	   String play = driver.findElement(By.xpath("//*[@class='mainsection']//*[@class='advanced_playlist']/li/a")).getAttribute("style");
+	   if (play.equals("")) {
+		   System.out.println("На странице "+driver.getCurrentUrl()+" не кликает на кнопку <<play>>");
+		   Reporter.log("На странице "+driver.getCurrentUrl()+" не кликает на кнопку <<play>>");
+		   throw new NullPointerException (); 
+	   }
 	   for(int i=0; i<=35; i++) {
 	   String styl = driver.findElement(By.xpath(".//*[@id='progress_slider']/a")).getAttribute("style");   
 	   if(styl.equals("left: 0%;")) {    
 	   Thread.sleep(1000);
 	   } else break;
 	   if(i>=30) { //set how long wait
-	    	 System.out.println("На странице "+driver.getCurrentUrl()+" не играет трек");
-		     Reporter.log("На странице "+driver.getCurrentUrl()+" не играет трек");
-		   throw new NullPointerException ();
+	    	System.out.println("На странице "+driver.getCurrentUrl()+" не играет трек");
+		    Reporter.log("На странице "+driver.getCurrentUrl()+" не играет трек");
+		    throw new NullPointerException ();
 	   }   
 	   }
 	   }
